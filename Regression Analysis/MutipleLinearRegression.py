91e86a1aa07f66cad3078f712cea0dd02e0dac6a
Ahsan <ahsanfarooq3423@gmail.com>
2019-09-23 18:21:39 +0500

    Backward Elimination to choose the best predictors for regression analysis

--- Regression Analysis/MutipleLinearRegression.py
+++ Regression Analysis/MutipleLinearRegression.py
@@ -13,7 +13,16 @@ X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,4].values
 
 
-from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:,3] = labelencoder_X.fit_transform(X[:,3])
+onehotencoder = OneHotEncoder(categorical_features= [3])
+X = onehotencoder.fit_transform(X).toarray()
 
+
+
+ 
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
 
